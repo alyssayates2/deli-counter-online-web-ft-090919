fbe4384ac5ff461683e3caeb40b3cafd53964265
Alyssa Yates <bosslys@icloud.com>
2019-09-13 17:42:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,8 +6,8 @@ def line(array)
     puts  "The line is currently empty."
 
     numbers = 1.
-  elsif array.length > 0 
-    numbers += 1 
+  elsif array.length > 0
+    numbers += 1
     array[i] += 1
     puts "The line is currently: #{numbers} #{array[0]}, "
 
