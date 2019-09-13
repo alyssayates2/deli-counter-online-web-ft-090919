912bc489b111345e7a94ad05c1ed3d219906a837
Alyssa Yates <bosslys@icloud.com>
2019-09-13 17:56:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 katz_deli = []
 
 
-def line(array)
+def line(array) 
   if array.length >= 1
     nuarray = []
     counter = 1
@@ -14,3 +14,8 @@ def line(array)
     puts "The line is currently empty."
   end
 end
+
+def take_a_number
+  katz_deli.push("#{counter}. #{name}")
+end 
+
